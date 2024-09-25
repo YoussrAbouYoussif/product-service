7269b5d6669bfa33496c0dfa8eb79fc7c2e92234
Youssr Abouyoussif <Youssr.Abouyoussif@itworx.com>
2024-09-25 07:34:03 +0300

    Add JUnits

---
+++ src/test/java/com/udin/tech/task/product_service/api/ProductApiControllerTest.java
@@ -0,0 +1,135 @@
+package com.udin.tech.task.product_service.api;
+
+import com.udin.tech.task.product_service.dao.ProductRepository;
+import com.udin.tech.task.product_service.model.request.CreateProductRequest;
+import com.udin.tech.task.product_service.model.response.ProductResponse;
+import com.udin.tech.task.product_service.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    public void setUp() {
+        productRepository.deleteAll();
+    }
+
+    @Test
+    public void testCreateProduct() throws Exception {
+        CreateProductRequest createProductRequest = new CreateProductRequest();
+        createProductRequest.setName("Laptop");
+        createProductRequest.setDescription("HP Laptop");
+
+        mockMvc.perform(post("/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"Laptop\", \"description\": \"HP Laptop\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Laptop"))
+                .andExpect(jsonPath("$.description").value("HP Laptop"));
+    }
+
+    @Test
+    void testGetAllProducts() throws Exception {
+        CreateProductRequest req1 = new CreateProductRequest();
+        req1.setName("Laptop");
+        req1.setDescription("HP Laptop");
+        productService.createProduct(req1);
+
+        CreateProductRequest req2 = new CreateProductRequest();
+        req2.setName("Iphone");
+        req2.setDescription("Iphone 15");
+        ProductResponse res2 = productService.createProduct(req2);
+
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Laptop"))
+                .andExpect(jsonPath("$[1].name").value("Iphone"));
+    }
+
+    @Test
+    void testGetProductById() throws Exception {
+        CreateProductRequest req1 = new CreateProductRequest();
+        req1.setName("Laptop");
+        req1.setDescription("HP Laptop");
+        ProductResponse res1 = productService.createProduct(req1);
+
+        CreateProductRequest req2 = new CreateProductRequest();
+        req2.setName("Iphone");
+        req2.setDescription("Iphone 15");
+        ProductResponse res2 = productService.createProduct(req2);
+
+        mockMvc.perform(get("/products/{id}", res2.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value("Iphone"));;
+    }
+
+    @Test
+    void testUpdateProductByIdSuccess() throws Exception {
+        CreateProductRequest req1 = new CreateProductRequest();
+        req1.setName("Laptop");
+        req1.setDescription("HP Laptop");
+        ProductResponse res1 = productService.createProduct(req1);
+
+        CreateProductRequest req2 = new CreateProductRequest();
+        req2.setName("Iphone");
+        req2.setDescription("Iphone 15");
+        ProductResponse res2 = productService.createProduct(req2);
+
+        mockMvc.perform(put("/products/{id}", res2.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"description\":\"Iphone 16\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.description").value("Iphone 16"));
+    }
+
+    @Test
+    void testUpdateProductByIdWithNullCriteria() throws Exception {
+
+        Long id = 0L;
+
+        mockMvc.perform(put("/products/{id}",  id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("Update criteria (name and price) cannot be null."));
+    }
+
+    @Test
+    void testDeleteProductById() throws Exception {
+        CreateProductRequest req1 = new CreateProductRequest();
+        req1.setName("Laptop");
+        req1.setDescription("HP Laptop");
+        ProductResponse res1 = productService.createProduct(req1);
+
+        CreateProductRequest req2 = new CreateProductRequest();
+        req2.setName("Iphone");
+        req2.setDescription("Iphone 15");
+        ProductResponse res2 = productService.createProduct(req2);
+
+        mockMvc.perform(delete("/products/{id}", res2.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
---
+++ src/test/java/com/udin/tech/task/product_service/service/ProductServiceImplTest.java
@@ -0,0 +1,174 @@
+package com.udin.tech.task.product_service.service;
+
+import com.udin.tech.task.product_service.dao.ProductRepository;
+import com.udin.tech.task.product_service.exception.ProductNotFoundException;
+import com.udin.tech.task.product_service.mapper.ProductsMapper;
+import com.udin.tech.task.product_service.model.request.CreateProductRequest;
+import com.udin.tech.task.product_service.model.request.UpdateProductRequest;
+import com.udin.tech.task.product_service.model.response.ProductResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class ProductServiceImplTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductsMapper mapper;
+
+    @Autowired
+    private ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+        productRepository.deleteAll();
+    }
+
+    @Test
+    void testCreateProductSuccess() {
+        CreateProductRequest createProductRequest = new CreateProductRequest();
+        createProductRequest.setName("Laptop");
+        createProductRequest.setDescription("HP Laptop");
+
+        ProductResponse response = productService.createProduct(createProductRequest);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getName()).isEqualTo("Laptop");
+        assertThat(response.getDescription()).isEqualTo("HP Laptop");
+    }
+
+    @Test
+    void testCreateProductFailure() {
+        CreateProductRequest createProductRequest = new CreateProductRequest();
+        createProductRequest.setName(null);
+        createProductRequest.setDescription("HP Laptop");
+
+        ConstraintViolationException exception = assertThrows(ConstraintViolationException.class, () -> {
+            productService.createProduct(createProductRequest);
+        });
+
+        assertThat(exception.getMessage()).contains("Name should not be empty.");
+    }
+
+    @Test
+    void testGetAllProducts() {
+        CreateProductRequest request1 = new CreateProductRequest();
+        request1.setName("Laptop");
+        request1.setDescription("HP Laptop");
+        productService.createProduct(request1);
+
+        CreateProductRequest request2 = new CreateProductRequest();
+        request2.setName("Iphone");
+        request2.setDescription("Iphone 15");
+        productService.createProduct(request2);
+
+        List<ProductResponse> products = productService.getAllProducts();
+
+        assertThat(products).hasSize(2);
+        assertThat(products).extracting(ProductResponse::getName).containsExactlyInAnyOrder("Laptop", "Iphone");
+    }
+
+    @Test
+    void testGetProductByIdSuccess() {
+        CreateProductRequest request1 = new CreateProductRequest();
+        request1.setName("Laptop");
+        request1.setDescription("HP Laptop");
+        ProductResponse res1 = productService.createProduct(request1);
+
+        CreateProductRequest request2 = new CreateProductRequest();
+        request2.setName("Iphone");
+        request2.setDescription("Iphone 15");
+        ProductResponse res2 = productService.createProduct(request2);
+
+        ProductResponse productResponse = productService.getProductById(res2.getId());
+
+        assertEquals(res2.getId(), productResponse.getId());
+        assertEquals(res2.getName(), productResponse.getName());
+    }
+
+    @Test
+    void testGetProductByIdFailure() {
+        Long nonExistentId = 999L;
+        ProductNotFoundException exception = assertThrows(ProductNotFoundException.class, () -> {
+            productService.getProductById(nonExistentId);
+        });
+
+        assertEquals("Product not found with id: " + nonExistentId, exception.getMessage());
+    }
+
+    @Test
+    void testUpdateProductByIdSuccess() {
+        CreateProductRequest request1 = new CreateProductRequest();
+        request1.setName("Laptop");
+        request1.setDescription("HP Laptop");
+        ProductResponse res1 = productService.createProduct(request1);
+
+        CreateProductRequest request2 = new CreateProductRequest();
+        request2.setName("Iphone");
+        request2.setDescription("Iphone 15");
+        ProductResponse res2 = productService.createProduct(request2);
+
+        UpdateProductRequest updateReq = new UpdateProductRequest();
+        updateReq.setDescription("Iphone 16");
+        ProductResponse productResponse = productService.updateProduct(res2.getId(), updateReq);
+
+        assertEquals(productResponse.getId(), res2.getId());
+        assertEquals(productResponse.getName(), res2.getName());
+        assertEquals(productResponse.getDescription(), updateReq.getDescription());
+    }
+
+    @Test
+    void testUpdateProductByIdFailure() {
+        UpdateProductRequest updateReq = new UpdateProductRequest();
+        updateReq.setDescription("Iphone 16");
+
+        Long nonExistentId = 999L;
+        ProductNotFoundException exception = assertThrows(ProductNotFoundException.class, () -> {
+            productService.updateProduct(nonExistentId, updateReq);
+        });
+
+        assertEquals("Product not found with id: " + nonExistentId, exception.getMessage());
+    }
+
+    @Test
+    void testDeleteProductByIdSuccess() {
+        CreateProductRequest request1 = new CreateProductRequest();
+        request1.setName("Laptop");
+        request1.setDescription("HP Laptop");
+        ProductResponse res1 = productService.createProduct(request1);
+
+        CreateProductRequest request2 = new CreateProductRequest();
+        request2.setName("Iphone");
+        request2.setDescription("Iphone 15");
+        ProductResponse res2 = productService.createProduct(request2);
+
+        productService.deleteProduct(res2.getId());
+
+        assertFalse(productRepository.existsById(res2.getId()));
+    }
+
+    @Test
+    public void testDeleteProductByIdFailure() {
+        Long productId = 999L;
+
+        Exception exception = assertThrows(ProductNotFoundException.class, () -> {
+            productService.deleteProduct(productId);
+        });
+
+        // Verify the exception message
+        assertEquals("Product not found with id: " + productId, exception.getMessage());
+    }
+}
